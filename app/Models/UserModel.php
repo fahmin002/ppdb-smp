eb5f15bf1cd00e4eca9591dadf19c11b366f67e4
fahminabilmaulana <fahminabilmaulana@gmail.com>
2025-03-12 00:46:43 +0700

    "Planning up the work"

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,16 +5,17 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    protected $table = 'user'; // Sesuaikan dengan nama tabel di database
-    protected $primaryKey = 'id';
+    protected $table = "user"; // Sesuaikan dengan nama tabel di database
+    protected $primaryKey = "id";
     protected $useTimestamps = true;
-    protected $allowedFields = ['alamat', 'slug', 'tipe', 'kota'];
+    protected $allowedFields = ["alamat", "slug", "tipe", "kota"];
 
     // Metode pencarian
     public function search($keyword)
     {
-        return $this->table('user')->like('alamat', $keyword)
-                    ->orLike('tipe', $keyword)
-                    ->orLike('kota', $keyword);
+        return $this->table("user")
+            ->like("alamat", $keyword)
+            ->orLike("tipe", $keyword)
+            ->orLike("kota", $keyword);
     }
 }
